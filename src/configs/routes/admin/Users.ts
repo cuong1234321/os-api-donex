2daf09bf80018635f158c849065dc4310cb30d90
vanpn <vanpn@flextech.vn>
2022-03-14 15:24:37 +0700

    [feat] active, inactive collaborator, user

---
+++ src/configs/routes/admin/Users.ts
@@ -0,0 +1,50 @@
+import UserController from '@controllers/api/admin/UsersController';
+import { Router } from 'express';
+
+const router = Router();
+
+/**
+ * @openapi
+ * /a/users/{userId}/active:
+ *   patch:
+ *     tags:
+ *      - "[ADMIN] Users"
+ *     summary: Mở khóa TK user
+ *     parameters:
+ *      - in: path
+ *        name: "userId"
+ *        description: "userId"
+ *        type: "string"
+ *     responses:
+ *       200:
+ *         description: Return data.
+ *       500:
+ *         description: Lỗi không xác định
+ *     security:
+ *      - Bearer: []
+ */
+router.patch('/:userId/active', UserController.active);
+
+/**
+ * @openapi
+ * /a/users/{userId}/inactive:
+ *   patch:
+ *     tags:
+ *      - "[ADMIN] Users"
+ *     summary: Khóa TK user
+ *     parameters:
+ *      - in: path
+ *        name: "userId"
+ *        description: "userId"
+ *        type: "string"
+ *     responses:
+ *       200:
+ *         description: Return data.
+ *       500:
+ *         description: Lỗi không xác định
+ *     security:
+ *      - Bearer: []
+ */
+router.patch('/:userId/inactive', UserController.inactive);
+
+export default router;
