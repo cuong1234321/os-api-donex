f0725337ff74c96adcff2a05b88037ab5d34ced0
vanpn <vanpn@flextech.vn>
2022-03-25 16:07:38 +0700

    [fear] DON-166 list notification

--- src/controllers/api/admin/MarketingNotificationsController.ts
+++ src/controllers/api/admin/MarketingNotificationsController.ts
@@ -4,8 +4,29 @@ import MarketingNotificationsModel from '@models/marketingNotifications';
 import { Transaction } from 'sequelize/types';
 import sequelize from '@initializers/sequelize';
 import MarketingNotificationTargetsModel from '@models/marketingNotificationTargets';
+import settings from '@configs/settings';
 
 class MarketingNotificationsController {
+  public async index (req: Request, res: Response) {
+    try {
+      const page = req.query.page as string || '1';
+      const limit = parseInt(req.query.limit as string) || parseInt(settings.defaultPerPage);
+      const offset = (parseInt(page, 10) - 1) * limit;
+      const { freeWord, status, listTarget } = req.query;
+      const scope: any = [
+        'withOwner',
+        'withNotificationTarget',
+      ];
+      if (status) scope.push({ method: ['byStatus', status] });
+      if (listTarget) scope.push({ method: ['byTarget', listTarget] });
+      if (freeWord) scope.push({ method: ['byFreeWord', freeWord] });
+      const { count, rows } = await MarketingNotificationsModel.scope(scope).findAndCountAll({ limit, offset, distinct: true, col: 'MarketingNotificationsModel.id' });
+      sendSuccess(res, { notifications: rows, pagination: { total: count, page, perPage: limit } });
+    } catch (error) {
+      sendError(res, 500, error.message, error);
+    }
+  }
+
   public async create (req: Request, res: Response) {
     try {
       const admin = req.currentAdmin || { id: 1 };
