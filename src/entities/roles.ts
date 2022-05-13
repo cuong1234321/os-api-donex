2b9fd9d01275fd08128f836bca2fd3f8b1a825d4
cuongnv <cuongnv@flextech.asia>
2022-05-13 11:50:29 +0700

    [FEATURE]: role

---
+++ src/entities/roles.ts
@@ -0,0 +1,24 @@
+import { DataTypes } from 'sequelize';
+
+const RoleEntity = {
+  id: {
+    type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false,
+  },
+  title: {
+    type: DataTypes.STRING(255), allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING(255), allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+  },
+  deletedAt: {
+    type: DataTypes.DATE,
+  },
+};
+
+export default RoleEntity;
